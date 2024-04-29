7081e94689849935193d532d1c08adbc36186e46
Pau Alcázar <pau.alcazar11@gmail.com>
2024-04-30 00:30:59 +0200

    Session 1

---
+++ script.js
@@ -0,0 +1,4 @@
+// Ejemplo de código JavaScript para agregar interactividad
+console.log("¡Hola desde Marta!");
+
+// Puedes agregar más código JavaScript aquí para crear animaciones, juegos o interacciones con el usuario.
